ac5bff4636f4a326e2e9bdb4f3f50bc3298d37a1
woodcol <fengmm521@gmail.com>
2018-09-30 14:14:37 +0800

    整点报时

---
+++ timetool/timetool.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-09-29 20:07:19
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os,sys
+import time
+
+voicefiles = {'am':'AM1.wav','pm':'PM','lc':'AM0','ws':'EM','point':'POINT','0':'T00','1':'T01',
+            '2':'T02',
+            '3':'T03',
+            '4':'T04',
+            '5':'T05',
+            '6':'T06',
+            '7':'T07',
+            '8':'T08',
+            '9':'T09',
+            '1_':'T1',
+            '2_':'T2',
+            '10':'T10',
+            '20':'T20',
+            '30':'T30',
+            '40':'T40',
+            '50':'T50',
+            'now':'TIMENOW',
+            'min':'MIN'
+}
+
+import platform
+sysSystem = platform.system()
+
+
+
+#获取脚本路径
+def cur_file_dir():
+    pathx = sys.argv[0]
+    tmppath,_file = os.path.split(pathx)
+    if cmp(tmppath,'') == 0:
+        tmppath = sys.path[0]
+    #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
+    if os.path.isdir(tmppath):
+        return tmppath
+    elif os.path.isfile(tmppath):
+        return os.path.dirname(tmppath)
+
+def cmp(a,b):
+    return ((a>b)-(a<b))
+
+#获取父目录
+def GetParentPath(strPath):
+    if not strPath:
+        return None;
+    lsPath = os.path.split(strPath);
+    if lsPath[1]:
+        return lsPath[0];
+    lsPath = os.path.split(lsPath[0]);
+    return lsPath[0];
+
+#获取目录下的所有类型文件
+def getAllExtFile(pth,fromatx = ".erl"):
+    jsondir = pth
+    jsonfilelist = []
+    for root, _dirs, files in os.walk(jsondir):
+        for filex in files:          
+            #print filex
+            name,text = os.path.splitext(filex)
+            if cmp(text,fromatx) == 0:
+                jsonArr = []
+                rootdir = pth
+                dirx = root[len(rootdir):]
+                pathName = dirx +os.sep + filex
+                jsonArr.append(pathName)
+                (newPath,_name) = os.path.split(pathName)
+                jsonArr.append(newPath)
+                jsonArr.append(name)
+                jsonfilelist.append(jsonArr)
+            elif fromatx == ".*" :
+                jsonArr = []
+                rootdir = pth
+                dirx = root[len(rootdir):]
+                pathName = dirx +os.sep + filex
+                jsonArr.append(pathName)
+                (newPath,_name) = os.path.split(pathName)
+                jsonArr.append(newPath)
+                jsonArr.append(name)
+                jsonfilelist.append(jsonArr)
+    return jsonfilelist
+
+
+#获取一个目录下的所有子目录路径
+def getAllDirs(spth):
+    files = getAllExtFile(spth,'.*')
+    makedirstmp = []
+    isOK = True
+    # 分析所有要创建的目录
+    for d in files:
+        if d[1] != '/' and (not d[1] in makedirstmp): #创建未创建的目录层级
+            tmpdir = d[1][1:]
+            tmpleves = tmpdir.split('/')
+            alldirs = getAllLevelDirs(tmpleves)
+            for dtmp in alldirs:
+                if not dtmp in makedirstmp:
+                    makedirstmp.append(dtmp)
+    return makedirstmp
+#获取目录下的所有文件路径
+def getAllFiles(spth):
+    files = getAllExtFile(spth,'.*')
+    makedirstmp = []
+    isOK = True
+    # 分析所有要创建的目录
+    for d in files:
+        makedirstmp.append(d[0])
+    return makedirstmp
+
+
+def isFile(filename):
+    try:
+        with open(filename) as f:
+            return True
+    except IOError:
+        return False
+
+def finddir(arg,dirname,filenames):
+    name,text = os.path.split(dirname)
+    dirnametmp = str(dirname)
+    if text and text[0] == '.':
+        print dirname
+        print filenames
+        os.system('rm -r %s'%(dirname))
+        return
+    elif filenames:
+        for f in filenames:
+            if f[0] == '.' and isFile(dirname + f):
+                fpthtmp = dirname + f
+                if f.find(' '):
+                    nf = f.replace(' ','\ ')
+                    fpthtmp = dirname + nf
+                print dirname + f
+                os.system('rm  %s'%(fpthtmp))
+
+#删除所有pth目录下的所有"."开头的文件名和目录名
+def removeProjectAllHideDir(pth):
+    alldirs = getAllDirs(pth)
+    if not ('/' in alldirs):
+        alldirs.append('/')
+    for d in alldirs:
+        tmpth = pth + d
+        os.path.walk(tmpth, finddir, 0)
+
+
+
+#获取一个路径中所包含的所有目录及子目录
+def getAllLevelDirs(dirpths):
+    dirleves = []
+    dirtmp = ''
+    for d in dirpths:
+        dirtmp += '/' + d
+        dirleves.append(dirtmp)
+    return dirleves
+
+#在outpth目录下创建ndir路径中的所有目录，是否使用决对路径
+def makeDir(outpth,ndir):
+    tmpdir = ''
+    if ndir[0] == '/':
+        tmpdir = outpth + ndir
+    else:
+        tmpdir = outpth + '/' + ndir
+    print tmpdir
+    if not os.path.exists(tmpdir):
+        os.mkdir(tmpdir)
+
+# 创建一个目录下的所有子目录到另一个目录
+def createDirs(spth,tpth):
+    files = getAllExtFile(spth,'.*')
+    makedirstmp = []
+    isOK = True
+    # 分析所有要创建的目录
+    tmpfpth = fpth
+    for d in files:
+        if d[1] != '/' and (not d[1] in makedirstmp): #创建未创建的目录层级
+            tmpdir = d[1][1:]
+            tmpleves = tmpdir.split('/')
+            alldirs = getAllLevelDirs(tmpleves)
+            for dtmp in alldirs:
+                if not dtmp in makedirstmp:
+                    makeDir(tpth,dtmp)
+                    makedirstmp.append(dtmp)
+
+# 替换文件名
+def replaceFileName(path,sname,replaceStr,tostr):
+    a = sname
+    tmpname = a.replace(replaceStr, tostr)
+    outpath = path + tmpname
+    oldpath = path + sname
+    cmd = "mv %s %s"%(oldpath,outpath)
+    print cmd
+    os.system("mv %s %s"%(oldpath,outpath))
+
+# 替换目录下的文件名中某个字符串为其他字符串
+def renameDir(sdir,replacestr,tostr,exittype):
+    files = getAllExtFile(sdir,fromatx = exittype)
+    allfilepath = []
+    for f in files:
+        tmppath = sdir + f[1]
+        filename = f[2] + exittype
+        allfilepath.append([tmppath,filename])
+    for p in allfilepath:
+        replaceFileName(p[0], p[1], replacestr, tostr)
+
+#获取目录下的所有文件路径
+def getAllFilesWithExp(spth,exp):
+    files = getAllExtFile(spth,exp)
+    makedirstmp = []
+    isOK = True
+    # 分析所有要创建的目录
+    for d in files:
+        makedirstmp.append(d[0])
+    return makedirstmp
+
+def playWavFile(pth):
+    cmd = '/usr/bin/mplayer %s'%(pth)
+    if sysSystem == 'Windows':  
+        cmd = ''
+    elif sysSystem == 'Darwin':
+        cmd = '/usr/local/bin/play %s'%(pth)
+    os.system(cmd)
+
+def playNowTime(speakerFnames):
+
+    for fpth in speakerFnames:
+        playWavFile(fpth)
+
+def getFileName(pth):
+    pth1,_ = os.path.splitext(pth)
+    fpth,fname = os.path.split(pth1)
+    return fpth,fname
+
+
+
+def playtime(hour,pmin = 0):
+    if pmin == 0:
+        fpth = ''
+        if hour < 10:
+            fpth = 'wav/0%d.wav'%(hour)
+        else:
+            fpth = 'wav/%d.wav'%(hour)
+        playWavFile(fpth)
+
+
+speaktimes = [8,10,12,14,16,18,20]
+
+
+isTest = True
+def main():
+
+
+    while True:
+        time.sleep(1)
+        loctim = time.localtime()
+        #time.struct_time(tm_year=2015, tm_mon=8, tm_mday=2, tm_hour=12, tm_min=16, tm_sec=47, tm_wday=6, tm_yday=214, tm_isdst=0)
+        if loctim.tm_min == 0 and loctim.tm_sec == 0 and loctim.tm_hour in speaktimes:
+            playtime(loctim.tm_hour)
+        elif isTest:
+            playtime(loctim.tm_hour)
+            break
+
+
+#测试
+if __name__ == '__main__':
+    main()
+
